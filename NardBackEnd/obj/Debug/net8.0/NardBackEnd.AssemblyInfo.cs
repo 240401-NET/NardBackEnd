14cd10aa99f3d4d512fb843046fa2f3bc35c7178
Stooge <batman.adamwes@gmail.com>
2024-05-07 21:29:35 -0400

    fixed null power error

--- NardBackEnd/obj/Debug/net8.0/NardBackEnd.AssemblyInfo.cs
+++ NardBackEnd/obj/Debug/net8.0/NardBackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NardBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2223f07aacd3c3aef51196f6beb9f210073d0c83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d5ab92e3e83a3d58b52e0b9574e6b8b80bb9130")]
 [assembly: System.Reflection.AssemblyProductAttribute("NardBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NardBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
